fdc7af36ca14db87e559361ca15c70be8db4b753
DESKTOP-DRCSSPA\Zintis <zintismay@gmail.com>
2022-08-27 20:17:36 -0400

    assignment updates

--- homework.js
+++ homework.js
@@ -1,23 +1,23 @@
-// Question 1 ///////////////////////////////////////////
-// create a variable "x", and do not assign it a value
+// #1 ///////////////////////////////////////////
+// Create a variable "x", and do not assign it a value.
 
-// Question 2 ///////////////////////////////////////////
-// create a variable "b", and give it the value of true
+// #2 ///////////////////////////////////////////
+// Create a variable "b", and give it the value of true.
 
-// Question 3 ///////////////////////////////////////////
-// create a variable "fruit" and give it eh value of "apple"
+// #3 ///////////////////////////////////////////
+// Create a variable "fruit" and give it eh value of "apple".
 
-// Question 4 ///////////////////////////////////////////
-// create a variable "arr3", and give it the value of an array. Make sure the array has 3 items in it
+// #4 ///////////////////////////////////////////
+// Create a variable "arr3", and give it the value of an array. Make sure the array has 3 items in it.
 
-// Question 5 ///////////////////////////////////////////
-// create a function called add that takes 2 parameters
-// add must return the sum of the two parameters
+// #5 ///////////////////////////////////////////
+// Create a function called "add" that takes 2 parameters
+// add must return the sum of the two parameters.
 
-// Question 6 ///////////////////////////////////////////
-// create a function called subtract that takes 2 parameters
-// subtract must return the first parameter minus the 2nd parameter
+// #6 ///////////////////////////////////////////
+// Create a function called "subtract" that takes 2 parameters
+// subtract must return the first parameter minus the 2nd parameter.
 
-// Question 7 ///////////////////////////////////////////
-// create a function called numberToString that takes 1 parameter
-// it must return the number, converted to the data type string
+// #7 ///////////////////////////////////////////
+// Create a function called "numberToString" that takes 1 parameter
+// it must return the number, converted to the data type string.
