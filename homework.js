f82352bc5769e0c06aa72ad384067b42668cbfa2
DESKTOP-DRCSSPA\Zintis <zintismay@gmail.com>
2022-08-27 21:03:17 -0400

    removed solutions

--- homework.js
+++ homework.js
@@ -1,59 +1,37 @@
 // #1 ///////////////////////////////////////////
 // Create a variable "x", and do not assign it a value.
-var x;
 
 // #2 ///////////////////////////////////////////
 // Create a variable "b", and give it the value of true.
-var b = true;
 
 // #3 ///////////////////////////////////////////
 // Create a variable "fruit" and give it eh value of "apple".
-var fruit = "apple";
 
 // #4 ///////////////////////////////////////////
 // Create a variable "arr3", and give it the value of an array. Make sure the array has 3 items in it.
-var arr3 = [1, 2, 3];
 
 // #5 ///////////////////////////////////////////
 // Create a function called "add" that takes 2 parameters
 // add must return the sum of the two parameters.
-function add(a, b) {
-  return a + b;
-}
 
 // #6 ///////////////////////////////////////////
 // Create a function called "subtract" that takes 2 parameters
 // subtract must return the first parameter minus the 2nd parameter.
-function subtract(a, b) {
-  return a - b;
-}
 
 // #7 ///////////////////////////////////////////
 // Create a function called "multiply" that takes 2 parameters
 // multiply must return the first parameter times the 2nd parameter.
-function multiply(a, b) {
-  return a * b;
-}
 
 // #8 ///////////////////////////////////////////
 // Create a function called "divide" that takes 2 parameters
 // divide must return the first parameter divided by the 2nd parameter.
-function divide(a, b) {
-  return a / b;
-}
 
 // #9 ///////////////////////////////////////////
 // Create a function called "numberToString" that takes 1 parameter
 // it must return the number, converted to the data type string.
 // use the "String" function. You can find how here: https://www.w3schools.com/jsref/jsref_string.asp
-function numberToString(num) {
-  return String(num);
-}
 
 // #10 ///////////////////////////////////////////
 // Create a function called "stringToNumber" that takes 1 parameter
 // it must return the string, converted to the data type number.
 // use the "Number" function. You can find how here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
-function stringToNumber(str) {
-  return Number(str);
-}
