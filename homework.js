8bfef8af0c66fd1d5227b9daa2431920c5cfa2d8
DESKTOP-DRCSSPA\Zintis <zintismay@gmail.com>
2022-08-27 20:14:23 -0400

    homework with instructions

--- homework.js
+++ homework.js
@@ -1,13 +1,23 @@
-function add(a, b) {
-  return a + b;
-}
-function subtract(a, b) {
-  return a - b;
-}
-var fruit = "apple";
-var x;
-function numberToString(n) {
-  return String(n);
-}
-var arr3 = [1, 2, 3];
-var b = true;
+// Question 1 ///////////////////////////////////////////
+// create a variable "x", and do not assign it a value
+
+// Question 2 ///////////////////////////////////////////
+// create a variable "b", and give it the value of true
+
+// Question 3 ///////////////////////////////////////////
+// create a variable "fruit" and give it eh value of "apple"
+
+// Question 4 ///////////////////////////////////////////
+// create a variable "arr3", and give it the value of an array. Make sure the array has 3 items in it
+
+// Question 5 ///////////////////////////////////////////
+// create a function called add that takes 2 parameters
+// add must return the sum of the two parameters
+
+// Question 6 ///////////////////////////////////////////
+// create a function called subtract that takes 2 parameters
+// subtract must return the first parameter minus the 2nd parameter
+
+// Question 7 ///////////////////////////////////////////
+// create a function called numberToString that takes 1 parameter
+// it must return the number, converted to the data type string
