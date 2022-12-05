6550c61719d442f33719bd29545f20407f382df7
AleksisLesieur <Aleksisjonaslesieur@gmail.com>
2022-12-05 16:56:16 +0200

    Exercises B exercise 3

---
+++ exercises-B/exercise3/code.js
@@ -0,0 +1,20 @@
+function returnANumber() {
+  return 5;
+  // Value not important only type
+}
+function returnAString() {
+  return "Hello";
+  // Value not important only type
+}
+function returnTrue() {
+  return true;
+}
+function returnFalse() {
+  return false;
+}
+function returnArray() {
+  return [];
+}
+function returnObject() {
+  return {};
+}
--- exercises-B/exercise3/tests.js
+++ exercises-B/exercise3/tests.js
@@ -1,3 +1,201 @@
-const testSuite = {};
+let zTestSuite = {};
 
-Z_T.testAll(testSuite);
+zTestSuite.returnANumberTests = {
+  title: `function "returnANumber"`,
+  instructions: `Make a function named "returnANumber" that returns a number (any number).`,
+  tests: [
+    {
+      description: `is declared`,
+      test: () => {
+        expect(returnANumber).toBeDeclared();
+      },
+    },
+    {
+      description: `has a value`,
+      test: () => {
+        expect(returnANumber).toHaveValue();
+      },
+    },
+    {
+      description: `is a function`,
+      test: () => {
+        expect(returnANumber).toBeFunction();
+      },
+    },
+    {
+      description: `returns a number`,
+      test: () => {
+        expect(returnANumber).toReturnNumber();
+      },
+    },
+  ],
+};
+
+zTestSuite.returnAStringTests = {
+  title: `function "returnAString"`,
+  instructions: `Make a function named "returnAString" that returns a string (any string).`,
+  tests: [
+    {
+      description: `is declared`,
+      test: () => {
+        expect(returnAString).toBeDeclared();
+      },
+    },
+    {
+      description: `has a value`,
+      test: () => {
+        expect(returnAString).toHaveValue();
+      },
+    },
+    {
+      description: `is a function`,
+      test: () => {
+        expect(returnAString).toBeFunction();
+      },
+    },
+    {
+      description: `returns a string`,
+      test: () => {
+        expect(returnAString).toReturnString();
+      },
+    },
+  ],
+};
+
+zTestSuite.returnTrueTests = {
+  title: `function "returnTrue"`,
+  instructions: `Make a function named "returnTrue" that returns the boolean true.`,
+  tests: [
+    {
+      description: `is declared`,
+      test: () => {
+        expect(returnTrue).toBeDeclared();
+      },
+    },
+    {
+      description: `has a value`,
+      test: () => {
+        expect(returnTrue).toHaveValue();
+      },
+    },
+    {
+      description: `is a function`,
+      test: () => {
+        expect(returnTrue).toBeFunction();
+      },
+    },
+    {
+      description: `returns a boolean`,
+      test: () => {
+        expect(returnTrue).toReturnBoolean();
+      },
+    },
+    {
+      description: `returns correct value true`,
+      test: () => {
+        expect(returnTrue).toReturn(true);
+      },
+    },
+  ],
+};
+
+zTestSuite.returnFalseTests = {
+  title: `function "returnFalse"`,
+  instructions: `Make a function named "returnFalse" that returns the boolean false.`,
+  tests: [
+    {
+      description: `is declared`,
+      test: () => {
+        expect(returnFalse).toBeDeclared();
+      },
+    },
+    {
+      description: `has a value`,
+      test: () => {
+        expect(returnFalse).toHaveValue();
+      },
+    },
+    {
+      description: `is a function`,
+      test: () => {
+        expect(returnFalse).toBeFunction();
+      },
+    },
+    {
+      description: `returns a boolean`,
+      test: () => {
+        expect(returnFalse).toReturnBoolean();
+      },
+    },
+    {
+      description: `returns correct value false`,
+      test: () => {
+        expect(returnFalse).toReturn(false);
+      },
+    },
+  ],
+};
+
+zTestSuite.returnArrayTests = {
+  title: `function "returnArray"`,
+  instructions: `Make a function named "returnArray" that returns an array.`,
+  tests: [
+    {
+      description: `is declared`,
+      test: () => {
+        expect(returnArray).toBeDeclared();
+      },
+    },
+    {
+      description: `has a value`,
+      test: () => {
+        expect(returnArray).toHaveValue();
+      },
+    },
+    {
+      description: `is a function`,
+      test: () => {
+        expect(returnArray).toBeFunction();
+      },
+    },
+    {
+      description: `returns an array`,
+      test: () => {
+        expect(returnArray).toReturnArray();
+      },
+    },
+  ],
+};
+
+zTestSuite.returnObjectTests = {
+  title: `function "returnObject"`,
+  instructions: `Make a function named "returnObject" that returns an object.`,
+  tests: [
+    {
+      description: `is declared`,
+      test: () => {
+        expect(returnObject).toBeDeclared();
+      },
+    },
+    {
+      description: `has a value`,
+      test: () => {
+        expect(returnObject).toHaveValue();
+      },
+    },
+    {
+      description: `is a function`,
+      test: () => {
+        expect(returnObject).toBeFunction();
+      },
+    },
+    {
+      description: `returns an object`,
+      test: () => {
+        expect(returnObject).toReturnObject();
+      },
+    },
+  ],
+};
+
+Z_T.testAll(zTestSuite);
