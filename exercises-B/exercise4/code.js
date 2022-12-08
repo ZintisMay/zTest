ba973b3d2a39fb64d6d9ec015c9980d8f7b89d66
AleksisLesieur <Aleksisjonaslesieur@gmail.com>
2022-12-08 22:44:14 +0200

    Exercises B exercise 4

---
+++ exercises-B/exercise4/code.js
@@ -0,0 +1,24 @@
+function addition(x, y) {
+  return x + y;
+}
+function subtraction(x, y) {
+  return x - y;
+}
+function multiplication(x, y) {
+  return x * y;
+}
+function division(x, y) {
+  return x / y;
+}
+function exponential(x, y) {
+  return x ** y;
+}
+function addStringsTogether(x, y) {
+  return x.concat(y);
+}
+function combineTwoArrays(x, y) {
+  return x.concat(y);
+}
+function combineTwoObjects(x, y) {
+  return Object.assign(x, y);
+}
