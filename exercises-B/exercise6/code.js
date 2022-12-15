4cee38e06a55871a164b1c5bf04e589ed933fa42
DESKTOP-DRCSSPA\Zintis <zintismay@gmail.com>
2022-12-14 20:17:01 -0500

    student release 2, many files removed

--- exercises-B/exercise6/code.js
+++
@@ -1,6 +0,0 @@
-var zeroArray = [];
-var easyArray = [4, 5, 6];
-var snacks = ["strawberry", "mango", "apple"];
-var smallNumbers = [8000, 7000, 6000];
-var arrayOfStrings = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
-var booleansArray = [true, true, false, false, true];
